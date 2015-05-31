1c4378a46bc80853655a4d0108ed943bccbcc4f8
4541Controls <4541controls@gmail.com>
2015-05-30 21:06:20 -0400

    Create MotorManip.java

---
+++ MotorManip.java
@@ -0,0 +1,25 @@
+//motor manipulator class
+import edu.wpi.first.wpilibj.Talon;
+
+public class MotorManip{
+	protected CANTalon t1;
+	
+	public MotorManip(int deviceID) {
+		t1 = new CANTalon(int deviceID));
+	}
+	
+	//sets the motor forward at a given speed
+	protected void motorForward(double speed) {
+		t1.set(speed);
+	}
+
+	//sets the motor in reverse at a given speed
+	protected void motorReverse(double speed) {
+		t1.set(-speed);
+	}
+	
+	//stops the motor
+	protected void motorStop() {
+		t1.set(0);
+	}
+}
