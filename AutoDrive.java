962afb73cc94e2e26c5806aea51e9da7fdd8a9b1
4541Controls <4541controls@gmail.com>
2015-05-30 21:04:55 -0400

    Create AutoDrive.java

---
+++ AutoDrive.java
@@ -0,0 +1,46 @@
+//autonomous drive class
+import org.usfirst.frc.team4541.driveSystems.MecanumDrive;
+
+public class AutoDrive {
+	MecanumDrive drive;
+	
+	public AutoDrive(MecanumDrive d) {
+		drive = d;
+	}
+	
+	//drives forward at quarter speed
+	public void forwardDrive() {
+		drive.setSpeed(0);
+		drive.drive(0, -1, 0);
+	}
+	
+	//drives backward at quarter speed
+	public void backwardDrive() {
+		drive.setSpeed(0);
+		drive.drive(0, 1, 0);
+	}
+	
+	//drives left at quarter speed
+	public void leftDrive() {
+		drive.setSpeed(0);
+		drive.drive(-1, 0, 0);
+	}
+	
+	//drives right at quarter speed
+	public void rightDrive() {
+		drive.setSpeed(0);
+		drive.drive(1, 0, 0);
+	}
+	
+	//turns left at quarter speed
+	public void leftTurn(){
+		drive.setSpeed(0);
+		drive.drive(0,0,-1);
+	}
+	
+	//turns right at quarter speed
+	public void rightTurn(){
+		drive.setSpeed(0);
+		drive.drive(0,0,1);
+	}
+}
