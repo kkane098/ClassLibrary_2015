fbaba90a362e19bc5d4532952aa9a1e9ce3300f6
kkane098 <kkane098@gmail.com>
2015-06-10 13:38:43 -0400

    Update AutoDrive.java

--- AutoDrive.java
+++ AutoDrive.java
@@ -3,31 +3,38 @@ import org.usfirst.frc.team4541.driveSystems.MecanumDrive;
 
 public class AutoDrive {
 	MecanumDrive drive;
-	
-	public AutoDrive(MecanumDrive d) {
+	PIDController pid;
+	public AutoDrive(MecanumDrive d, PIDController p){
 		drive = d;
+		pid = p;
+	}
+	
+	public void pidDrive(double distance, double minIn, double maxIn){
+		pid.setInputRange(minIn, maxIn);
+		pid.setSetpoint(distance);
+		pid.run();
 	}
 	
 	//drives forward at quarter speed
-	public void forwardDrive() {
+	public void forwardDrive(){
 		drive.setSpeed(0);
 		drive.drive(0, -1, 0);
 	}
 	
 	//drives backward at quarter speed
-	public void backwardDrive() {
+	public void backwardDrive(){
 		drive.setSpeed(0);
 		drive.drive(0, 1, 0);
 	}
 	
 	//drives left at quarter speed
-	public void leftDrive() {
+	public void leftDrive(){
 		drive.setSpeed(0);
 		drive.drive(-1, 0, 0);
 	}
 	
 	//drives right at quarter speed
-	public void rightDrive() {
+	public void rightDrive(){
 		drive.setSpeed(0);
 		drive.drive(1, 0, 0);
 	}
